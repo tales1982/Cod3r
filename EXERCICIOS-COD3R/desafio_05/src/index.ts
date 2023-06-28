7b8589671c916e3ae576205587e97832d3ad8524
tales1982 <tales_lima_1982@hotmail.com>
2023-06-28 21:47:08 +0200

    Desavio criando tipos

---
+++ EXERCICIOS-COD3R/desafio_05/src/index.ts
@@ -0,0 +1,26 @@
+type ContaBancaria = {
+    saldo: number,
+    depositar: (valor: number) => void
+}
+
+let contaBancaria: ContaBancaria = {
+    saldo: 3456,
+    depositar(valor: number) {
+        this.saldo += valor
+    }
+}
+
+type Correntista = {
+    nome: string,
+    contaBancaria: ContaBancaria,
+    contatos: string[]
+}
+
+let correntista: Correntista = {
+    nome: 'Ana Silva',
+    contaBancaria: contaBancaria,
+    contatos: ['34567890', '98765432']
+}
+
+correntista.contaBancaria.depositar(3000)
+console.log(correntista)
